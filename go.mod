9ddda964f0eb9d00c5e7feae59f7de4630068886
mavolin <48887425+mavolin@users.noreply.github.com>
2021-02-08 21:00:07 +0100

    feat(i18nwrapper): add i18n wrapper

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mavolin/levin
 
-go 1.14
+go 1.16
 
 require (
 	github.com/diamondburned/arikawa/v2 v2.0.2
@@ -8,10 +8,9 @@ require (
 	github.com/iancoleman/strcase v0.1.3
 	github.com/mavolin/adam v0.0.0-20210206171900-edf32277f1b8
 	github.com/mavolin/disstate/v3 v3.1.1
+	github.com/nicksnyder/go-i18n/v2 v2.1.2
 	github.com/spf13/jwalterweatherman v1.0.0
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.7.0 // indirect
 	go.uber.org/zap v1.10.0
-	golang.org/x/text v0.3.3 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	golang.org/x/text v0.3.3
 )
---
+++ internal/i18nwrapper/load.go
@@ -0,0 +1,175 @@
+package i18nwrapper
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/fs"
+	"os"
+	"regexp"
+
+	i18nimpl "github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+
+	"github.com/mavolin/levin/assets"
+)
+
+// Load loads the embedded translation files.
+// Optionally, if customPath isn't empty it will load additional translations
+// found int the passed path.
+// All non-translation files will be skipped.
+func Load(b *i18nimpl.Bundle, customPath string) error {
+	err := loadEmbeddedTranslations(b)
+	if err != nil {
+		return err
+	}
+
+	if len(customPath) > 0 {
+		return loadCustomTranslations(b, customPath)
+	}
+
+	return nil
+}
+
+type (
+	translation struct {
+		Term string `json:"term"`
+		// Definition is either a string or definition
+		Definition json.RawMessage `json:"definition"`
+	}
+
+	// definition is the type used, if there are multiple definitions.
+	definition struct {
+		Zero  string `json:"zero"`
+		One   string `json:"one"`
+		Two   string `json:"two"`
+		Few   string `json:"few"`
+		Many  string `json:"many"`
+		Other string `json:"other"`
+	}
+)
+
+func (d *definition) isEmpty() bool {
+	return len(d.Zero) == 0 && len(d.One) == 0 && len(d.Two) == 0 &&
+		len(d.Few) == 0 && len(d.Many) == 0 && len(d.Other) == 0
+}
+
+var (
+	eFileRegexp  = regexp.MustCompile(`^(?P<lang>.+?)(?:_(?:adam|levin))?\.json$`)
+	owFileRegexp = regexp.MustCompile(`^(?P<lang>.+?)\.json$`)
+)
+
+func loadEmbeddedTranslations(b *i18nimpl.Bundle) error {
+	dir, err := assets.Translations.ReadDir("translations")
+	if err != nil {
+		return err
+	}
+
+	for _, f := range dir {
+		if f.IsDir() {
+			continue
+		}
+
+		matches := eFileRegexp.FindStringSubmatch(f.Name())
+		if len(matches) < 2 {
+			log().With("file_name", f.Name()).
+				Warn("found non-translation file in embedded translations, skipping")
+		}
+
+		tag, err := language.Parse(matches[1])
+		if err != nil {
+			log().With("lang", matches[1]).
+				Warn("embedded translations contain a translation file for invalid language, skipping")
+		}
+
+		f, err := assets.Translations.Open("translations/" + f.Name())
+		if err != nil {
+			return err
+		}
+
+		if err = loadTranslation(b, tag, f); err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func loadCustomTranslations(b *i18nimpl.Bundle, customPath string) error {
+	dir, err := os.Open(customPath)
+	if err != nil {
+		return err
+	}
+
+	files, err := dir.Readdir(0)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		matches := owFileRegexp.FindStringSubmatch(f.Name())
+		if len(matches) < 2 {
+			continue
+		}
+
+		tag, err := language.Parse(matches[1])
+		if err != nil {
+			continue
+		}
+
+		f, err := assets.Translations.Open(customPath + f.Name())
+		if err != nil {
+			return err
+		}
+
+		if err = loadTranslation(b, tag, f); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func loadTranslation(b *i18nimpl.Bundle, tag language.Tag, f fs.File) error {
+	var messages []translation
+
+	if err := json.NewDecoder(f).Decode(&messages); err != nil {
+		return err
+	}
+
+	for _, m := range messages {
+		var def definition
+
+		if bytes.HasPrefix(m.Definition, []byte(`"`)) {
+			if err := json.Unmarshal(m.Definition, &def.Other); err != nil {
+				return err
+			}
+		} else { // definition object
+			if err := json.Unmarshal(m.Definition, &def); err != nil {
+				return err
+			}
+		}
+
+		if len(m.Term) == 0 || def.isEmpty() {
+			continue
+		}
+
+		err := b.AddMessages(tag, &i18nimpl.Message{
+			ID:    m.Term,
+			Zero:  def.Zero,
+			One:   def.One,
+			Two:   def.Two,
+			Few:   def.Few,
+			Many:  def.Many,
+			Other: def.Other,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
