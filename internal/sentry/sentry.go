c881bed79fb3ff2cf598371a2a2cabe5d9ea0233
mavolin <48887425+mavolin@users.noreply.github.com>
2021-02-11 00:14:21 +0100

    refactor(sentry): rename Get to GetHub and prevent nil return

--- internal/sentry/sentry.go
+++ internal/sentry/sentry.go
@@ -2,15 +2,10 @@ package sentry
 
 import (
 	"github.com/getsentry/sentry-go"
-	"github.com/mavolin/adam/pkg/bot"
-	"github.com/mavolin/adam/pkg/plugin"
-	"github.com/mavolin/disstate/v3/pkg/state"
 
 	"github.com/mavolin/levin/internal/config"
 	"github.com/mavolin/levin/internal/meta"
 )
-
-const hubKey = "sentry.hub"
 
 // Init initializes sentry using config.C.
 func Init() error {
@@ -25,44 +20,18 @@ func Init() error {
 	})
 }
 
-// Get extracts the *sentry.Hub from the *plugin.Context.
-// This will only work, if a *sentry.Hub was previously stored in the Context
-// under 'sentry.hub', e.g. using NewMiddleware.
-func Get(ctx *plugin.Context) *sentry.Hub {
-	return ctx.Get(hubKey).(*sentry.Hub)
-}
-
-// NewMiddleware creates a new middleware usable by adam using the passed
-// *sentry.Hub.
-// For each invocation, it creates a clone of the *sentry.Hub and attaches
-// meta information about the invoke.
-// Afterwards, it stores the clone in the command's context under 'sentry.hub'.
-func NewMiddleware(h *sentry.Hub) bot.MiddlewareFunc {
-	return func(next bot.CommandFunc) bot.CommandFunc {
-		return func(s *state.State, ctx *plugin.Context) error {
-			h := h.Clone()
-
-			h.WithScope(func(s *sentry.Scope) {
-				s.SetTag("err_source", "bot")
-			})
-
-			h.AddBreadcrumb(&sentry.Breadcrumb{
-				Category: "bot.router",
-				Message:  "routed command",
-				Data: map[string]interface{}{
-					"command_provider": ctx.InvokedCommand.ProviderName,
-					"command_id":       ctx.InvokedCommand.Identifier,
-					"message":          ctx.Message,
-					"lang":             ctx.Lang,
-					"invoker_id":       ctx.Author.ID,
-					"message_id":       ctx.Message.ID,
-					"channel_id":       ctx.ChannelID,
-					"guild_id":         ctx.GuildID,
-				},
-			}, nil)
-			ctx.Set(hubKey, h)
-
-			return next(s, ctx)
+// GetHub extracts the *sentry.Hub from the passed context.
+// The context is abstracted as interface { Get(string) interface{} }, to allow
+// both getting from *state.XEvents and *plugin.Contexts.
+//
+// If no hub is accessible using 'sentry.hub' as key, a clone of the current
+// hub will be returned.
+func GetHub(ctx interface{ Get(string) interface{} }) *sentry.Hub {
+	if hub := ctx.Get(hubKey); hub != nil {
+		if hub, ok := hub.(*sentry.Hub); ok && hub != nil {
+			return hub
 		}
 	}
+
+	return sentry.CurrentHub().Clone()
 }
